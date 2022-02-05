90e530597fc12383e3a6d4669e1c207b9b543d8a
Mikhail <44241285+Mikhail-Mouner@users.noreply.github.com>
2022-02-05 21:48:33 +0200

    Finish Task

--- app/autoload.php
+++ app/autoload.php
@@ -1,5 +1,14 @@
 <?php
-function __autoload($class)
+
+spl_autoload_register('Autoloader::ClassLoader');
+
+class Autoloader
 {
-    require "{$class}.php";
+    const DS = DIRECTORY_SEPARATOR;
+
+    public static function ClassLoader($class)
+    {
+        $class = str_replace('\\', self::DS, $class);
+        require_once "{$class}.php";
+    }
 }
